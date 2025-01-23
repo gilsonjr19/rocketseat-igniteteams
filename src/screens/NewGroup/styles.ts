471a11f50b1d364c3cd6223ef9a53b5639ef47cc
gilsonjr19 <sazevedojunior@icloud.com>
2025-01-23 18:32:04 -0500

    feat: Create NewGroup screen.

---
+++ src/screens/NewGroup/styles.ts
@@ -0,0 +1,21 @@
+import theme from '@/theme';
+import styled from 'styled-components/native';
+import { IconUsersGroup } from '@tabler/icons-react-native';
+
+export const Container = styled.View`
+    flex: 1;
+    background-color: ${theme.COLORS.GRAY_600};
+    padding: 24px;
+`;
+
+export const Content = styled.View`
+    flex: 1;
+    justify-content: center;
+`;
+
+export const Icon = styled(IconUsersGroup).attrs({
+    size: 56,
+    color: theme.COLORS.GREEN_700,
+})`
+    align-self: center;
+`;
