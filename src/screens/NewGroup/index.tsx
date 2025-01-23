7203d3e74ff2e4e7121c5f4f2c98e4a6ca76c616
gilsonjr19 <sazevedojunior@icloud.com>
2025-01-23 18:45:33 -0500

    feat: Create Input component.

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -3,6 +3,7 @@ import { Container, Content, Icon } from './styles';
 import { Header } from '@/components/Header';
 import { Highlight } from '@/components/Highlight';
 import { Button } from '@/components/Button';
+import { Input } from '@/components/Input';
 
 export function NewGroup() {
     return (
@@ -15,8 +16,12 @@ export function NewGroup() {
                     title="Nova turma"
                     subtitle="crie a turma para adicionar as pessoass"
                 />
+
+                <Input />
+
                 <Button
                     title="Criar"
+                    style={{ marginTop: 20 }}
                 />
             </Content>
         </Container>
