57d59ba156480e3f642af86c24155c1979917ea5
gilsonjr19 <sazevedojunior@icloud.com>
2025-01-23 00:12:18 -0500

    feat: Create Button component.

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -7,6 +7,7 @@ import { GroupCard } from '@/components/GroupCard';
 
 import { Container } from './styles';
 import { ListEmpty } from '@/components/ListEmpty';
+import { Button } from '@/components/Button';
 
 export function Groups() {
     const [groups, setGroups] = useState<string[]>([]);
@@ -30,6 +31,10 @@ export function Groups() {
                 )}
                 contentContainerStyle={groups.length === 0 && { flex: 1 }}
                 ListEmptyComponent={() => <ListEmpty message="Que tal cadastrar a primeira turma?" />}
+            />
+
+            <Button 
+                title="Criar nova turma"
             />
 
         </Container>
