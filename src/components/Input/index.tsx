4c280b1d1472b5635e41f3b0b1bc4fbb3ca984c8
gilsonjr19 <sazevedojunior@icloud.com>
2025-01-23 22:28:51 -0500

    feat: Create Players screen.

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,9 +1,13 @@
 import { TextInputProps } from 'react-native';
 import { Container } from './styles';
+import theme from '@/theme';
 
 export function Input({ ...rest }: TextInputProps) {
+    const { COLORS } = theme;
+
     return (
         <Container
+            placeholderTextColor={COLORS.GRAY_300}
             {...rest}
         />
     );
--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,5 +1,5 @@
-import { TextInput } from 'react-native';
 import theme from '@/theme';
+import { TextInput } from 'react-native';
 import styled from 'styled-components/native';
 
 export const Container = styled(TextInput)`
