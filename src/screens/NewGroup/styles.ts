f04c09fe6b7d7a0bfbaa405cbacb3478b1defca8
gilsonjr19 <sazevedojunior@icloud.com>
2025-04-03 23:50:53 -0500

    style: update to SafeAreaView (Safe Area Context).

--- src/screens/NewGroup/styles.ts
+++ src/screens/NewGroup/styles.ts
@@ -1,8 +1,9 @@
+import { SafeAreaView } from 'react-native-safe-area-context';
 import theme from '@/theme';
 import styled from 'styled-components/native';
 import { IconUsersGroup } from '@tabler/icons-react-native';
 
-export const Container = styled.View`
+export const Container = styled(SafeAreaView)`
     flex: 1;
     background-color: ${theme.COLORS.GRAY_600};
     padding: 24px;
